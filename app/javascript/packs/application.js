1a36b692173815f64bae948891fbeae3f5697271
pavemen3 <43461016+pavemen3@users.noreply.github.com>
2022-04-20 12:09:59 +0900

    webpacker 導入

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,10 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+// import "bootstrap"
+
+import '../javascripts/application.js'
+import '../stylesheets/application.scss'
 
 Rails.start()
 Turbolinks.start()
